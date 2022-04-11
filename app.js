3bc9dca4ca8782743fe4b13d9edf3e878c21d308
maryoud2000 <maryoud2000@gmail.com>
2022-04-11 22:45:48 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 65) {
+        a.pause();
+        a.currentTime = 0;
+        a.play();  
+    }
+    if (e.keyCode == 83) {
+        s.pause();
+        s.currentTime = 0;
+        s.play();
+    }
+    if (e.keyCode == 68) {
+        d.pause();
+        d.currentTime = 0;
+        d.play();
+    }
+    if (e.keyCode == 70) {
+        f.pause();
+        f.currentTime = 0;
+        f.play();
+    }
+    if (e.keyCode == 71) {
+        g.pause();
+        g.currentTime = 0;
+        g.play();
+    }
+    if (e.keyCode == 72) {
+        h.pause();
+        h.currentTime = 0;
+        h.play();
+    }
+    if (e.keyCode == 74) {
+        j.pause();
+        j.currentTime = 0;
+        j.play();
+    }
+    if (e.keyCode == 75) {
+        k.pause();
+        k.currentTime = 0;
+        k.play();
+    }
+    if (e.keyCode == 76) {
+        l.pause();
+        l.currentTime = 0;
+        l.play();
+    }
+  });
+
+  
